5c8a7c3aea7bd271d80009a9d811cf7be68d7b76
zhaoxianglinXL <15736535817@163.com>
2018-11-29 17:52:39 +0800

    Eureka Demo基本完成

---
+++ microservicecloud-eurake-7001/src/main/java/com/winner/springcloud/EurekaServer7001_APP.java
@@ -0,0 +1,15 @@
+package com.winner.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//申明我是微服务注册中心启动类  EurekaServer服务端启动类，接受其他服务注册进来
+@EnableEurekaServer//在主启动类上面，标注的启动该新组件技术的相关注解标签
+public class EurekaServer7001_APP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer7001_APP.class, args);
+	}
+}
