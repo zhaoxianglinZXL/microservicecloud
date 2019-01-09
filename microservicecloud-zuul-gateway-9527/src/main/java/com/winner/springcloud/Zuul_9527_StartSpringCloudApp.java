782a7542daa20a5131e56e16da615dc0511026c8
zhaoxianglinXL <15736535817@163.com>
2019-01-09 15:35:57 +0800

    ZUUL网络网关的初步使用 实现了访问假的微服务名来访问服务

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/winner/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,14 @@
+package com.winner.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy //申明他是zuull路由网关代理
+public class Zuul_9527_StartSpringCloudApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+	}
+}
